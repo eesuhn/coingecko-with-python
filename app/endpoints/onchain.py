cb5af9fd464a6cf1e5f0ba982b845004d94472c1
eesuhn <eason.yihong@gmail.com>
2025-01-24 14:47:25 +0800

    Onchain top pools by token address

--- app/endpoints/onchain.py
+++ app/endpoints/onchain.py
@@ -1,7 +1,15 @@
+from typing import Union
+from enum import Enum
+
 from ..utils import cg_request
 
 
 class Onchain:
+    class SortTopPoolsByTokenAddress(Enum):
+        H24_VOLUME_USD_LIQUIDITY_DESC = "h24_volume_usd_liquidity_desc"
+        H24_TX_COUNT_DESC = "h24_tx_count_desc"
+        H24_VOLUME_USD_DESC = "h24_volume_usd_desc"
+
     def token_data_by_token_address(
         self,
         network: str,
@@ -11,4 +19,27 @@ class Onchain:
         endpoint = f"/onchain/networks/{network}/tokens/{token_address}"
         if top_pools:
             endpoint += "?include=top_pools"
+        return cg_request(endpoint)
+
+    def top_pools_by_token_address(
+        self,
+        network: str,
+        token_address: str,
+        base_token: bool = False,
+        quote_token: bool = False,
+        dex: bool = False,
+        sort: Union[SortTopPoolsByTokenAddress, None] = None,
+    ) -> dict:
+        endpoint = f"/onchain/networks/{network}/tokens/{token_address}/pools"
+        if base_token or quote_token or dex:
+            endpoint += "?include="
+            if base_token:
+                endpoint += "base_token%2C"
+            if quote_token:
+                endpoint += "quote_token%2C"
+            if dex:
+                endpoint += "dex%2C"
+            endpoint = endpoint[:-3]
+        if sort:
+            endpoint += f"?sort={sort.value}"
         return cg_request(endpoint)
--- app/main.py
+++ app/main.py
@@ -11,11 +11,34 @@ class Main:
             self._run_onchain()
 
     def _run_onchain(self) -> None:
+        self.network = "avax"
+        self.token_address = "0x2b2c81e08f1af8835a78bb2a90ae924ace0ea4be"
         self.onchain = Onchain()
-        response = self.onchain.token_data_by_token_address(
-            network="avax",
-            token_address="0x2b2c81e08f1af8835a78bb2a90ae924ace0ea4be",
+
+        self._run_onchain_token_data_by_token_address()
+        self._run_onchain_top_pools_by_token_address()
+
+    def _run_onchain_token_data_by_token_address(self) -> None:
+        res_onchain_token_data_by_token_address = self.onchain.token_data_by_token_address(
+            network=self.network,
+            token_address=self.token_address,
             top_pools=True
         )
-        print_json(response)
-        # log_json(response, "onchain_token_data_by_token_address", prod_filename=True)
+        print_json(res_onchain_token_data_by_token_address)
+        log_json(
+            res_onchain_token_data_by_token_address,
+            "onchain_token_data_by_token_address",
+            prod_filename=True
+        )
+
+    def _run_onchain_top_pools_by_token_address(self) -> None:
+        res_onchain_top_pools_by_token_address = self.onchain.top_pools_by_token_address(
+            network=self.network,
+            token_address=self.token_address,
+        )
+        print_json(res_onchain_top_pools_by_token_address)
+        log_json(
+            res_onchain_top_pools_by_token_address,
+            "onchain_top_pools_by_token_address",
+            prod_filename=True
+        )
